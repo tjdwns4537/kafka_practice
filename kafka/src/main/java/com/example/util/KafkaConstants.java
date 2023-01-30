8af507d48741c025d6aa94def5691f647479c91a
tjdwns4537 <tjdwns4537@naver.com>
2023-01-31 02:48:37 +0900

    refact: 주석제거

--- kafka/src/main/java/com/example/util/KafkaConstants.java
+++ kafka/src/main/java/com/example/util/KafkaConstants.java
@@ -9,6 +9,5 @@ public class KafkaConstants {
      * */
     public static final String KAFKA_TOPIC = "kafka-chat"; // 생성한 토픽의 이름
     public static final String GROUP_ID = "chatting"; // consumer를 식별할 수 있는 그룹
-//    public static final String KAFKA_BROKER = "localhost:9092"; // Kafka 클러스터에 연결하기 위한 호스트:포트 값
     public static final String KAFKA_BROKER = "52.201.218.52:9092"; // Kafka 클러스터에 연결하기 위한 호스트:포트 값
 }
